b9f481939a4ab2e10119faf1aec6522a7ec74b9f
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-04-05 07:44:37 -0400

    continued working on vision

--- visiontest.py
+++ visiontest.py
@@ -1,6 +1,7 @@
 import io
 import os
 import cv2
+import time
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -17,26 +18,33 @@ cap = cv2.VideoCapture(0)
 # The name of the image file to annotate
 file_name = 'images/live.jpg'
 
+frame_counter = 0
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    frame_counter += 1
 
-    # cv2.imwrite(file_name, frame)
+    if frame_counter > 29:
+        cv2.imwrite(file_name, frame)
 
-    # # Loads the image into memory
-    # with io.open(file_name, 'rb') as image_file:
-    #     content = image_file.read()
+        # Loads the image into memory
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
 
-    # image = types.Image(content=content)
+        image = types.Image(content=content)
 
-    # # Performs label detection on the image file
-    # response = client.label_detection(image=image)
-    # labels = response.label_annotations
+        # Performs label detection on the image file
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
 
-    # for label in labels:
-    #     print(label.description, label.score)
+        for label in labels:
+            print(label.description, label.score)
+
+        frame_counter = 0
 
     cv2.imshow('currFrame', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
