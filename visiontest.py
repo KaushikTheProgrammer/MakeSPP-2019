e0fbadbbf035dd182416d56123ce836d140e49e5
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-04-06 09:47:03 -0400

    Test multithreading is killing me plz halp

--- visiontest.py
+++ visiontest.py
@@ -2,6 +2,7 @@ import io
 import os
 import cv2
 import time
+import threading 
 
 # Imports the Google Cloud client library
 from google.cloud import vision
@@ -13,41 +14,73 @@ credentials = service_account.Credentials.from_service_account_file('MakeSPP.jso
 # Instantiates a client
 client = vision.ImageAnnotatorClient(credentials=credentials)
 
-cap = cv2.VideoCapture(0)
+def showCamera():
+    cap = cv2.VideoCapture(0)
 
-# The name of the image file to annotate
-file_name = 'images/live.jpg'
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-frame_counter = 0
+        cv2.imshow('currFrame', frame)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame_counter += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    if frame_counter > 29:
-        cv2.imwrite(file_name, frame)
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
-        # Loads the image into memory
-        with io.open(file_name, 'rb') as image_file:
-            content = image_file.read()
+def analyzeFrame(frame):
 
-        image = types.Image(content=content)
+    # The name of the image file to annotate
+    file_name = 'images/live.jpg'
 
-        # Performs label detection on the image file
-        response = client.label_detection(image=image)
-        labels = response.label_annotations
+    frame_counter = 0
 
-        for label in labels:
-            print(label.description, label.score)
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        frame_counter += 1
 
-        frame_counter = 0
+        if frame_counter > 29:
+            cv2.imwrite(file_name, frame)
 
-    cv2.imshow('currFrame', frame)
+            # Loads the image into memory
+            with io.open(file_name, 'rb') as image_file:
+                content = image_file.read()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            image = types.Image(content=content)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+            # Performs label detection on the image file
+            response = client.label_detection(image=image)
+            labels = response.label_annotations
+
+            print(type(labels))
+
+            frame_counter = 0
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__": 
+	# creating thread 
+	t1 = threading.Thread(target=showCamera) 
+	# t2 = threading.Thread(target=analyzeFrames) 
+
+	# starting thread 1 
+	t1.start() 
+	# starting thread 2 
+	# t2.start() 
+
+	# wait until thread 1 is completely executed 
+	t1.join() 
+	# wait until thread 2 is completely executed 
+	# t2.join() 
+
+	# both threads completely executed 
+	print("Done!") 
