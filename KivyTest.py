c2f17af0ac39ed988e98b7ffdf48c5cc9d52e32d
mwolak07 <40680140+mwolak07@users.noreply.github.com>
2019-04-06 12:49:09 -0400

    Added live video to image

--- KivyTest.py
+++ KivyTest.py
@@ -3,6 +3,10 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from kivy.uix.image import Image
+import cv2
+import threading
+
+file_name = "image.jpg"
 
 
 # Definition of kivy App instance
@@ -14,10 +18,10 @@ class DisplayWindow(App):
         # Layout of window to contain image and label attributes
         self.layout = BoxLayout(orientation="vertical")
         self.title_text = Label(text="Title", size_hint=(1, .1))
-        self.image = Image(source="1.jpg", size_hint=(1, .7))
+        self.image = Image(source="image.jpg", size_hint=(1, .7))
         self.output = Label(text="one", size_hint=(1, .1))
         # Dynamic callbacks scheduled with Clock to display video feed and analysis
-        Clock.schedule_interval(self.videoCallback, 1/60)
+        Clock.schedule_interval(self.videoCallback, 1/10)
         Clock.schedule_interval(self.labelCallback, 1)
         # Adding attributes to box as widgets
         self.layout.add_widget(self.title_text)
@@ -28,12 +32,7 @@ class DisplayWindow(App):
     # Reloads image from the disk with specified refresh rate
     def videoCallback(self, dt):
         # Change to image.reload() in final
-        if(self.thing):
-            self.image.source = "1.jpg"
-            self.thing = False
-        else:
-            self.image.source = "2.jpg"
-            self.thing = True
+        self.image.reload()
 
     # Refreshes label at specified time interval, checking for change in detection boolean
     def labelCallback(self, dt):
@@ -43,5 +42,35 @@ class DisplayWindow(App):
             self.output.text = "two"
 
 
-# Running kivy app
-DisplayWindow().run()
+def userInterface():
+    DisplayWindow().run()
+
+
+if __name__ == "__main__":
+
+    t1 = threading.Thread(target=userInterface)
+    t1.start()
+
+    frame_counter = 0
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        frame_counter += 1
+
+        # cv2.imshow('frame', frame)
+
+        cv2.imwrite(file_name, frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            isQuit = True
+            break
+
+    t1.join()
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+    print("done")
