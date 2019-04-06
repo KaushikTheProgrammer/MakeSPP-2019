a1e2c94549c3e464d9dcd41e7209790a7460b288
mwolak07 <40680140+mwolak07@users.noreply.github.com>
2019-04-06 11:17:30 -0400

    Kivy UI w/ varied refresh

--- KivyTest.py
+++ KivyTest.py
@@ -1,23 +1,34 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.button import Button
-from functools import partial
 from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.uix.image import Image
 
 
-class KivyButton(App):
-    def disable(self, instance, *args):
-        instance.disabled = True
-
-    def update(self, instance, *args):
-        instance.text = "I am disabled!"
-
+class thiccBoi(App):
+    thing = False
     def build(self):
-        mybtn = Button(text="click me to disable!", background_color=(1, 0, 1, 1), pos=(300, 350), size_hint=(0.25, 0.18))
-        mybtn.bind(on_press=partial(self.disable, mybtn))
-        mybtn.bind(on_press=partial(self.update, mybtn))
-        return mybtn
+        self.box = BoxLayout(orientation='horizontal', spacing=20)
+        self.image = Image(source="1.jpg")
+        self.mylabel = Label(text="one")
+        Clock.schedule_interval(self.Clock_Callback, 1/60)
+        Clock.schedule_interval(self.Clock_Callback2, 1)
+        self.box.add_widget(self.image)
+        self.box.add_widget(self.mylabel)
+        return self.box
+    def Clock_Callback(self, dt):
+        # Change to image.reload() in final
+        if(self.thing):
+            self.image.source = "1.jpg"
+            self.thing = False
+        else:
+            self.image.source = "2.jpg"
+            self.thing = True
+    def Clock_Callback2(self, dt):
+        if(self.thing):
+            self.mylabel.text = "one"
+        else:
+            self.mylabel.text = "two"
 
 
-KivyButton().run()
+thiccBoi().run()
